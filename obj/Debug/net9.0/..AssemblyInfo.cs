23d2cfde735add8da0bedb85043dcfc618a1cd65
Bruno Alves <brunoalvesmisael542@gmail.com>
2025-08-21 16:36:42 -0300

    .gitignore

--- obj/Debug/net9.0/..AssemblyInfo.cs
+++ obj/Debug/net9.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca92f6fb772828a517074591ad373ffbe82d523")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a3a2137c8fe59b5302fa080b748a8892c821ad5")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
